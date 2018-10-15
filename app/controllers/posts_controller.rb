0e7b490aec3a9a127e506d4c9a8c5d6ab480c050
Sidra Kidwai <sidra.kidwai@square63.org>
2018-10-15 17:48:24 +0500

    finished installing and integrating bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,8 @@ class PostsController < ApplicationController
       @posts = Post.all
     else
       @category_id = Category.find_by(name: params[:category]).id
+      @posts = Post.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
