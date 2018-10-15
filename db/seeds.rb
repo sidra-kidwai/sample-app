4baf1567db5ef3f656a20d0e1306c89dde161266
Sidra Kidwai <sidra.kidwai@square63.org>
2018-10-15 12:54:41 +0500

    finished the Post model and Posts controller actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: 'Fiction')
+Category.create(name: 'Science')
+Category.create(name: 'Information Technology')
+Category.create(name: 'Knowledger')
